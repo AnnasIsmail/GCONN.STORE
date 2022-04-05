58876bb9cddd2dd796c4533ecb1f0c55b8ee8c19
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-04-05 15:55:20 +0700

    update navbar

--- src/Component/NavbarBeforeLogin/NavbarBeforeLogin.jsx
+++ src/Component/NavbarBeforeLogin/NavbarBeforeLogin.jsx
@@ -4,8 +4,12 @@ import './NavbarBeforeLogin.css';
 function NavbarBeforeLogin(){
     return(
         <div>
-            <span>
-                <img className='logo-gconn-before-navbar' src={logoGconn} alt="" />
+            <span className='logo-gconn-before-navbar'>
+                <img src={logoGconn} alt="" />
+                <span>
+                    <h1>GCONN</h1>
+                    <h3>LEADING GAMES ACCOUNT MARKETPLACE</h3>
+                </span>
             </span>
         </div>
     );
