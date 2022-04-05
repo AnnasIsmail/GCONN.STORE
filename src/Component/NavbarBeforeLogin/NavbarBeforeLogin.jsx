c2013f04c1e65dfb26b2d78072d0a536751e1723
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-04-05 22:39:20 +0700

    Navbar

--- src/Component/NavbarBeforeLogin/NavbarBeforeLogin.jsx
+++ src/Component/NavbarBeforeLogin/NavbarBeforeLogin.jsx
@@ -2,16 +2,27 @@ import logoGconn from '../../image/logo-gconn-nobackground.png';
 import './NavbarBeforeLogin.css';
 
 function NavbarBeforeLogin(){
+    let mouseMasuk = ()=>[
+        
+    ]
+
     return(
         <div>
-            <span className='logo-gconn-before-navbar'>
+            <div className='logo-gconn-before-navbar'>
                 <img src={logoGconn} alt="" />
                 <span>
                     <h1>GCONN</h1>
                     <h3>LEADING GAMES ACCOUNT MARKETPLACE</h3>
                 </span>
-                
-            </span>
+                <span>
+                </span>
+            </div>
+                    <i class="gg-home-alt" id='home' onMouseEnter={mouseMasuk}></i>
+                    {/* <i class="gg-heart"></i>
+                    <i class="gg-shopping-bag"></i>
+                    <i class="gg-log-in"></i>
+                    <i class="gg-user-add"></i>
+                    <i class="gg-shopping-cart"></i> */}
         </div>
     );
 }
