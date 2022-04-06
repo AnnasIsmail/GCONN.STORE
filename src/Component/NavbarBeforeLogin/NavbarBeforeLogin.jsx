7974fb6472437f56fc952f1e51a854175cadff80
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-04-06 22:01:53 +0700

    update navbar

--- src/Component/NavbarBeforeLogin/NavbarBeforeLogin.jsx
+++ src/Component/NavbarBeforeLogin/NavbarBeforeLogin.jsx
@@ -1,19 +1,21 @@
-import HomeIcon from '../../image/icon/home';
+import HomeIcon from '../../image/icon/home/home';
+import MarketIcon from '../../image/icon/market/market';
 import logoGconn from '../../image/logo-gconn-nobackground.png';
 import './NavbarBeforeLogin.css';
 
 function NavbarBeforeLogin(){
 
     return(
-        <div>
-            <div className='logo-gconn-before-navbar'>
+        <div className='navbar'>
+            <span className='logo-gconn-before-navbar'>
                 <img src={logoGconn} alt="" />
                 <span>
                     <h1>GCONN</h1>
                     <h3>LEADING GAMES ACCOUNT MARKETPLACE</h3>
                 </span>
-            </div>
-            <HomeIcon />
+            </span>
+            <div><HomeIcon /> <h3>Home</h3></div>
+            <div><MarketIcon /> <h3>Market</h3></div>
         </div>
     );
 }
