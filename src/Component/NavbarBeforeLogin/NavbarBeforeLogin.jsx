ceaee8c48853646235e5a5436560452031c3970a
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-04-07 13:30:55 +0700

    update navbar

--- src/Component/NavbarBeforeLogin/NavbarBeforeLogin.jsx
+++ src/Component/NavbarBeforeLogin/NavbarBeforeLogin.jsx
@@ -1,5 +1,8 @@
-import HomeIcon from '../../image/icon/home/home';
-import MarketIcon from '../../image/icon/market/market';
+import HomeIcon from '../../image/icon/home';
+import LogInIcon from '../../image/icon/logIn';
+import MarketIcon from '../../image/icon/market';
+import SignUpIcon from '../../image/icon/SignUp';
+import StarIcon from '../../image/icon/star';
 import logoGconn from '../../image/logo-gconn-nobackground.png';
 import './NavbarBeforeLogin.css';
 
@@ -14,8 +17,11 @@ function NavbarBeforeLogin(){
                     <h3>LEADING GAMES ACCOUNT MARKETPLACE</h3>
                 </span>
             </span>
-            <div><HomeIcon /> <h3>Home</h3></div>
-            <div><MarketIcon /> <h3>Market</h3></div>
+            <div><HomeIcon diKlik="false" /> <h3>Home</h3></div>
+            <div><MarketIcon diKlik="false" /> <h3>Market</h3></div>
+            <div><StarIcon diKlik="false" /> <h3>Favourite</h3></div>
+            <div><LogInIcon diKlik="true" /> <h3>Log In</h3></div>
+            <div><SignUpIcon diKlik="false" /> <h3>Sign Up</h3></div>
         </div>
     );
 }
